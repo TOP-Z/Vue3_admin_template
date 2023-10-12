5d5e944b8c6157425cdba372558cf51eeced9d1d
Top-gitee <1607135344@qq.com>
2023-10-12 17:54:32 +0800

    接口类型规范

--- src/api/user/index.ts
+++ src/api/user/index.ts
@@ -2,11 +2,12 @@
  * @Description:
  * @Author: 振顺
  * @Date: 2023-10-12 16:46:16
- * @LastEditTime: 2023-10-12 16:47:04
+ * @LastEditTime: 2023-10-12 17:51:29
  * @LastEditors: 振顺
  */
 // 统一管理项目用户相关的接口
 import request from "@/utils/request";
+import type { LoginFrom, LoginRes, UserResponseData } from "./type";
 // 统一管理接口
 enum Api {
     LOGIN_URL = '/user/login',
@@ -14,3 +15,6 @@ enum Api {
 }
 // 暴露请求函数
 // 登录接口方法
+export const reqLogin = (data: LoginFrom) => request.post<any, LoginRes>(Api.LOGIN_URL, data);
+// 获取用户信息接口方法
+export const reqUserInfo = () => request.get<any, UserResponseData>(Api.USERINFO_URL);
---
+++ src/api/user/type.ts
@@ -0,0 +1,53 @@
+/*
+ * @Description: 
+ * @Author: 振顺
+ * @Date: 2023-10-12 16:46:36
+ * @LastEditTime: 2023-10-12 17:50:54
+ * @LastEditors: 振顺
+ */
+// 登录接口需要携带参数ts类型
+export interface LoginFrom {
+    username: string
+    password: string
+}
+
+interface dataType {
+    token: string
+    userInfo: {
+        id: number
+        username: string
+        nickname: string
+        email: string
+        phone: string
+        sex: number
+        role: number
+        createTime: string
+        updateTime: string
+    }
+}
+
+// 登录接口返回数据类型
+export interface LoginRes {
+    code: number
+    data: dataType
+}
+
+// 定义服务器返回用户信息相关的数据类型
+interface userInfo {
+    userId: number
+    avatar:string
+    username: string
+    password: string
+    desc:string
+    roles:string[]
+    buttons:string[]
+    routes:string[]
+    token:string
+}
+interface user {
+    checkUser:userInfo
+}
+export interface UserResponseData {
+    code: number
+    data: user
+}
